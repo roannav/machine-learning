34c1bac299ef2e2378e766d4e916d92807a2c28d
Roanna Victorio <roannav@users.noreply.github.com>
2022-06-10 19:04:28 -0700

    add selecting part of 2D array and using np.nonzero()

--- numpy/selecting.py
+++ numpy/selecting.py
@@ -29,3 +29,62 @@ print(cond, '\n')
 cond = (3 < arr) & (arr < 7)    # near the middle only
 print(arr[cond])
 print(cond, '\n')
+
+print("______________________________________________________________")
+
+print("Selecting part of a 2D array")
+print("arr[ row indicies, column indicies]")
+
+arr = np.array([[0, 1], [2, 3], [4, 5]])
+print(arr)
+
+print("\nThe first row:   arr[0, :]")
+print(arr[0, :])
+print(type(arr[0, :]))
+
+print("\nThe second row:   arr[1, :]")
+print(arr[1, :])
+
+print("\nThe last row:   arr[-1, :]")
+print(arr[1, :])
+
+print("\nThe first column:   arr[:, 0]")
+print(arr[:, 0])
+
+print("\nThe second column:   arr[:, 1]")
+print(arr[:, 1])
+
+
+print("______________________________________________________________")
+
+# Set up Earth with random values, representing different people.
+# 25 random integers from 0..99
+earth = np.random.randint(0, 100, 25)
+earth = np.reshape(earth, (5,5))
+print(earth)
+
+# Place Waldo in a random location on Earth
+WALDO = 1000
+Waldo_location = (np.random.randint(5), np.random.randint(5))
+print(Waldo_location)
+print(type(Waldo_location))
+
+earth[Waldo_location] = WALDO
+print(earth)
+
+# Find Waldo
+Waldo_loc = np.nonzero(earth == WALDO)
+print(Waldo_loc)
+
+# np.nonzero( cond) returns a tuple
+# The tuple contains np.ndim arrays.
+# The first array tells which rows met the condition.
+# The second array tells which columns met the condition.
+
+# So Waldo_loc contains the row and column  where Waldo is.
+
+# Let's confirm Waldo is there, by printing what is at the location
+print(earth[Waldo_loc])
+if earth[Waldo_loc] == WALDO:
+    print("Found Waldo")
+
